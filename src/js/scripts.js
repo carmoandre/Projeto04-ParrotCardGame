fe695520f235e328b204cf6aa795765d53898483
André Carmo <andre_de11@msn.com>
2021-04-09 12:30:37 -0300

    Refatorações na função que gerencia o funcionamento do jogo.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -25,7 +25,7 @@ for (let i = 0; i < numberOfCards/2; i++) {
 }
 
 for (let i = 0; i < numberOfCards; i++) {
-    document.querySelector(".cardDisplay").innerHTML += `<li onclick="turnCard(this)" id="${i+1}" class="card unturned"><img src="src/images/front.png" alt="desenho de um papagaio verde, de bico amarelo e laranja, e barriga vermelha e azul, usado como verso de todas as cartas, que é substituido pela figura de papagaio psicodélico escolhida aleatóriamente escondida depois que a carta é clicada"></li>`;
+    document.querySelector(".cardDisplay").innerHTML += `<li onclick="gamePlaying(this)" id="${i+1}" class="card unturned"><img src="src/images/front.png" alt="desenho de um papagaio verde, de bico amarelo e laranja, e barriga vermelha e azul, usado como verso de todas as cartas, que é substituido pela figura de papagaio psicodélico escolhida aleatóriamente escondida depois que a carta é clicada"></li>`;
 }
 
 gameList.sort(comparator);
@@ -35,52 +35,41 @@ function comparator() {
     return Math.random() - 0.5;
 }
 
-function turnCard(element) {
+function turnCard(cardElement, imageElement, imageSRC) {
+    imageElement.src = imageSRC;
+    cardElement.classList.toggle("unturned");
+}
+
+function gamePlaying(element) {
     const cardImage = element.querySelector("img");
-    console.log("ID da carta: " + element.id);
-
-
     
     if (element.classList.contains("unturned")) {
-        console.log("virou essa");
-        cardImage.src = gameList[element.id-1];
-        element.classList.toggle("unturned");
+        turnCard(element, cardImage, gameList[element.id-1]);
         numberOfTurns++;
-
     } 
+
     if (previousCard != "None" && !previousCard[0].classList.contains("matched") &&!previousCard[0].classList.contains("unturned") ) {
-        console.log("ID da carta anterior: " + previousCard[0].id);
-        console.log("ID da carta atual: " + element.id);
-        console.log("SRC da imagem anterior: " + previousCard[1].src);
-        console.log("SRC da imagem atual: " + cardImage.src); 
+        
         if ( previousCard[0].id != element.id && previousCard[1].src === cardImage.src) {
-            console.log("Matched!")
             previousCard[0].classList.add("matched");
             element.classList.add("matched");
             pairsMatched++;
             previousCard = [element, cardImage];
-            console.log("----------------------------");
+            
             if (pairsMatched === numberOfCards/2) {
                 setTimeout(alert, 50, `Você ganhou em ${numberOfTurns} jogadas`);
             }
             return;
         } 
-        if(previousCard[0].id === element.id) {
-            return;
-        }
-        console.log("Desvirou a ultima");
-        previousCard[1].src = coverGif;
-        previousCard[0].classList.toggle("unturned")
+        setTimeout(turnCard, 1000, previousCard[0], previousCard[1], coverGif);
         if( previousCard[0].id != element.id) {
-            console.log("Desvirou essa");
-            cardImage.src = coverGif;
-            element.classList.toggle("unturned");
+            setTimeout(turnCard, 1000, element, cardImage, coverGif);
+        } else {
+            previousCard = "None";
+            return;
         }
     } 
     
     previousCard = [element, cardImage];
-    if (pairsMatched === numberOfCards/2) {
-        setTimeout(alert, 50, `Você ganhou em ${numberOfTurns} jogadas`);
-    }
-    console.log("----------------------------");
 }
+
